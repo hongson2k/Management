5648666cc56fae24c563e68dfb186e6ad032b15d
SƠN VÕ NGUYỄN HỒNG <18120538@student.hcmus.edu.vn>
2021-04-03 16:43:54 +0700

    a

--- GiaoDien/Dashboard.Designer.cs
+++ GiaoDien/Dashboard.Designer.cs
@@ -29,165 +29,14 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
-            this.panelstatistic = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.stasticpicture = new System.Windows.Forms.PictureBox();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            this.txtRoleNumber = new System.Windows.Forms.TextBox();
-            this.txt_role = new System.Windows.Forms.TextBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.txt_soluonguser = new System.Windows.Forms.TextBox();
-            this.txt_userAmount = new System.Windows.Forms.TextBox();
             this.topdashboard_text = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.pn_User = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.panelstatistic.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.stasticpicture)).BeginInit();
-            this.panel4.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
-            this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.pn_User.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // panelstatistic
-            // 
-            this.panelstatistic.BackColor = System.Drawing.Color.Transparent;
-            this.panelstatistic.Controls.Add(this.textBox1);
-            this.panelstatistic.Controls.Add(this.stasticpicture);
-            this.panelstatistic.Location = new System.Drawing.Point(798, 74);
-            this.panelstatistic.Name = "panelstatistic";
-            this.panelstatistic.Size = new System.Drawing.Size(318, 382);
-            this.panelstatistic.TabIndex = 8;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.BackColor = System.Drawing.Color.White;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.Color.DarkViolet;
-            this.textBox1.Location = new System.Drawing.Point(93, 29);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(130, 42);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "67% ";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // stasticpicture
-            // 
-            this.stasticpicture.BackColor = System.Drawing.Color.White;
-            this.stasticpicture.Image = ((System.Drawing.Image)(resources.GetObject("stasticpicture.Image")));
-            this.stasticpicture.Location = new System.Drawing.Point(22, 73);
-            this.stasticpicture.Name = "stasticpicture";
-            this.stasticpicture.Size = new System.Drawing.Size(269, 243);
-            this.stasticpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.stasticpicture.TabIndex = 4;
-            this.stasticpicture.TabStop = false;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.Transparent;
-            this.panel4.Controls.Add(this.pictureBox6);
-            this.panel4.Controls.Add(this.txtRoleNumber);
-            this.panel4.Controls.Add(this.txt_role);
-            this.panel4.Location = new System.Drawing.Point(368, 74);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(424, 337);
-            this.panel4.TabIndex = 7;
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.BackColor = System.Drawing.Color.White;
-            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
-            this.pictureBox6.Location = new System.Drawing.Point(50, 73);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(302, 243);
-            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox6.TabIndex = 3;
-            this.pictureBox6.TabStop = false;
-            // 
-            // txtRoleNumber
-            // 
-            this.txtRoleNumber.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtRoleNumber.BackColor = System.Drawing.Color.White;
-            this.txtRoleNumber.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtRoleNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtRoleNumber.ForeColor = System.Drawing.Color.Red;
-            this.txtRoleNumber.Location = new System.Drawing.Point(94, 29);
-            this.txtRoleNumber.Name = "txtRoleNumber";
-            this.txtRoleNumber.Size = new System.Drawing.Size(54, 38);
-            this.txtRoleNumber.TabIndex = 2;
-            this.txtRoleNumber.Text = "10";
-            this.txtRoleNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // txt_role
-            // 
-            this.txt_role.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_role.BackColor = System.Drawing.Color.White;
-            this.txt_role.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_role.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_role.ForeColor = System.Drawing.Color.DarkViolet;
-            this.txt_role.Location = new System.Drawing.Point(154, 25);
-            this.txt_role.Name = "txt_role";
-            this.txt_role.Size = new System.Drawing.Size(130, 42);
-            this.txt_role.TabIndex = 1;
-            this.txt_role.Text = "Roles";
-            this.txt_role.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.Transparent;
-            this.panel3.Controls.Add(this.pictureBox2);
-            this.panel3.Controls.Add(this.txt_soluonguser);
-            this.panel3.Controls.Add(this.txt_userAmount);
-            this.panel3.Location = new System.Drawing.Point(24, 74);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(326, 337);
-            this.panel3.TabIndex = 6;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(24, 89);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(264, 227);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
-            // 
-            // txt_soluonguser
-            // 
-            this.txt_soluonguser.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_soluonguser.BackColor = System.Drawing.Color.White;
-            this.txt_soluonguser.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_soluonguser.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_soluonguser.ForeColor = System.Drawing.Color.MediumAquamarine;
-            this.txt_soluonguser.Location = new System.Drawing.Point(49, 29);
-            this.txt_soluonguser.Name = "txt_soluonguser";
-            this.txt_soluonguser.Size = new System.Drawing.Size(38, 38);
-            this.txt_soluonguser.TabIndex = 2;
-            this.txt_soluonguser.Text = "78";
-            this.txt_soluonguser.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // txt_userAmount
-            // 
-            this.txt_userAmount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txt_userAmount.BackColor = System.Drawing.Color.White;
-            this.txt_userAmount.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txt_userAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_userAmount.ForeColor = System.Drawing.Color.Coral;
-            this.txt_userAmount.Location = new System.Drawing.Point(102, 25);
-            this.txt_userAmount.Name = "txt_userAmount";
-            this.txt_userAmount.Size = new System.Drawing.Size(119, 42);
-            this.txt_userAmount.TabIndex = 1;
-            this.txt_userAmount.Text = "Users";
-            this.txt_userAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // topdashboard_text
             // 
@@ -206,8 +55,9 @@
             // pn_User
             // 
             this.pn_User.BackColor = System.Drawing.Color.DodgerBlue;
+            this.pn_User.Controls.Add(this.textBox1);
             this.pn_User.Controls.Add(this.pictureBox1);
-            this.pn_User.Location = new System.Drawing.Point(261, 444);
+            this.pn_User.Location = new System.Drawing.Point(63, 74);
             this.pn_User.Name = "pn_User";
             this.pn_User.Size = new System.Drawing.Size(200, 100);
             this.pn_User.TabIndex = 10;
@@ -215,12 +65,19 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(47, 16);
+            this.pictureBox1.Location = new System.Drawing.Point(54, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(104, 51);
+            this.pictureBox1.Size = new System.Drawing.Size(96, 62);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 11;
             // 
             // Dashboard
             // 
@@ -230,22 +87,11 @@
             this.ClientSize = new System.Drawing.Size(1268, 628);
             this.Controls.Add(this.pn_User);
             this.Controls.Add(this.topdashboard_text);
-            this.Controls.Add(this.panelstatistic);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Dashboard";
             this.Text = "Dashboard";
-            this.panelstatistic.ResumeLayout(false);
-            this.panelstatistic.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.stasticpicture)).EndInit();
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.pn_User.ResumeLayout(false);
+            this.pn_User.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -253,21 +99,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panelstatistic;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.PictureBox stasticpicture;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.PictureBox pictureBox6;
-        private System.Windows.Forms.TextBox txtRoleNumber;
-        private System.Windows.Forms.TextBox txt_role;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.TextBox txt_soluonguser;
-        private System.Windows.Forms.TextBox txt_userAmount;
         private System.Windows.Forms.TextBox topdashboard_text;
-        private System.Windows.Forms.Panel pn_User;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel pn_User;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
